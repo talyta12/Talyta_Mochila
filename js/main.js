21e3cee12f0081c98d4ca74fc6c5bbffb5be41e1
talytaaraujo <129244780+talyta12@users.noreply.github.com>
2023-11-27 19:20:57 -0300

    hyjeyj

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", ()=> {
      evento.preventDefault() 
 
-     evento.target[0].value
+     
+     console.log(evento)
      console.log(evento.target[0].value)
 })
