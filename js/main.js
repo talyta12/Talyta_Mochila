f64b3f680c03ed5a032de207f59a2ed57d7e7419
talytaaraujo <129244780+talyta12@users.noreply.github.com>
2023-11-27 19:48:02 -0300

    thth

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ const form = document.getElementById("novoItem")
 form.addEventlistener("submit", (evento) => { 
     evento.preventDefault()
 
-
-    console.log(evento)
     console.log(evento.target.elements['nome'].value)
     console.log(evento.target.elements['quantidade'].value)
+
+    criaElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
+    console.log(nome)
+    console.log(quantidade)
+
+ const novoItem =  document.createElement('li')
+ 
+
 })
 
+function criaElemento(nome, quantidade) {
+
+}
